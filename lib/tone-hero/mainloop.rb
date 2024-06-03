e59bc34f0f4b2020c6c44c79d752555f17395ee8
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-06-03 20:00:48 +0300

    sorta works

--- lib/tone-hero/mainloop.rb
+++ lib/tone-hero/mainloop.rb
@@ -3,8 +3,8 @@ require 'tone-hero/2d'
 
 module ToneHero
 
-include Ruby2D
-extend Ruby2D::DSL
+  include Ruby2D
+  extend Ruby2D::DSL
 
   class MainLoop
     def initialize(input)
@@ -14,30 +14,50 @@ extend Ruby2D::DSL
     end
 
 
+    def read_notes
+      loop do
+        events = @input.gets
+        # pp events
+        events.each do |e|
+          # pp e
+
+          code, note, _ = e[:data]
+          if(code == NoteOn)
+            @notes_on << note_name(note)
+            puts note_name(note)
+          end
+        end
+      end
+    end
+
+
     def run
+      # Window.update do
+      Thread.new { read_notes }
+
+      t = Time.now
       Window.update do
         if @tick % 60 == 0
           Window.set background: 'random'
+          puts "60 frams in #{Time.now - t} seconds"
+          t = Time.now
+          # pp @input
         end
         @tick += 1
-        # pp @notes_on
+
+        sleep 0.0001
+
+        # if(@notes_on.length > 0)
+        #   pp @notes_on
+        #   @notes_on = []
+        # end
+        #
+      # Thread.pas
+      # slee
+
       end
 
-      # Thread.new do
-        puts "Reading MIDI..."
-        loop do
-          m = @input.gets
-
-          msg, note, _ = m[0][:data]
-
-          if(msg == NoteOn) then
-            ToneHero.text "NoteOn: #{note_name(note)}"
-            @notes_on << note
-          end
-        end
-      # end
-
-      # Window.show
+      Window.show
 
     end
   end
--- lib/tone-hero/midi.rb
+++ lib/tone-hero/midi.rb
@@ -2,5 +2,5 @@ require 'unimidi'
 require 'tone-hero/midi/const'
 
 def open_midi_in
-  return UniMIDI::Input.open(0)
+  return UniMIDI::Input.open(1)
 end
