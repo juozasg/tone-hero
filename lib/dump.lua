a400b010c4a15b46a72a36d3f488cf9d097a3794
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-06-05 18:41:01 +0300

    dump.lua

---
+++ lib/dump.lua
@@ -0,0 +1,13 @@
+---@diagnostic disable-next-line: lowercase-global
+function dump(o)
+	if type(o) == 'table' then
+		 local s = '{ '
+		 for k,v in pairs(o) do
+				if type(k) ~= 'number' then k = '"'..k..'"' end
+				s = s .. '['..k..'] = ' .. dump(v) .. ','
+		 end
+		 return s .. '} '
+	else
+		 return tostring(o)
+	end
+end
